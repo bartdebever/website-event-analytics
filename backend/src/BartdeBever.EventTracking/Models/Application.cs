d1ac5c4b1ce4e79e3d345684a506fe646cc6fd7f
Bart de Bever <bartdebever112@gmail.com>
2026-01-05 23:17:29 +0100

    chore: Setup backend project

---
+++ backend/src/BartdeBever.EventTracking/Models/Application.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BartdeBever.EventTracking.Models;
+
+public class Application
+{
+    [Column("id")]
+    public Guid Id { get; set; }
+    
+    [Column("name")]
+    public string Name { get; set; }
+    
+    [Column("api_key")]
+    public string ApiKey { get; set; }
+    
+    [Column("schema_name")]
+    public string SchemaName { get; set; }
+}
---
+++ backend/src/BartdeBever.EventTracking/Models/EventLog.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BartdeBever.EventTracking.Models;
+
+public class EventLog
+{
+    [Column("id")]
+    public long Id { get; set; }
+    
+    [Column("event_name")]
+    public string EventName { get; set; }
+    
+    [Column("session_id")]
+    public Guid SessionId { get; set; }
+    
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+    
+    [Column("url")]
+    public string Url { get; set; }
+    
+    [Column("data")]
+    public object Data { get; set; }
+}
